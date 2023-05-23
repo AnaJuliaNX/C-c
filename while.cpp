ac4e78dbff8aba9f8c3b4c655a7b6c702637a231
AnaJuliaNX <88339570+AnaJuliaNX@users.noreply.github.com>
2023-05-23 20:33:18 -0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,17 @@
+//usada pra quando não sabe quantas vezes ao certo repetir
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <locale.h>
+
+main ()
+ { setlocale(LC_ALL, "Portuguese");
+  int contador = 1;
+   
+    while (contador <= 11)
+     {
+     	printf("\nBoa noite");
+     	contador++;
+	 }
+}
