15fef5eff4fcadea1a18aa11620ca6927f62c264
AnaJuliaNX <88339570+AnaJuliaNX@users.noreply.github.com>
2023-06-06 20:42:57 -0300

    Add files via upload

---
+++ atleta.cpp
@@ -0,0 +1,20 @@
+#include <conio.h>
+#include <math.h>
+#include <locale.h>
+
+main ()
+ { setlocale(LC_ALL, "Portuguese");
+ int i, idade;
+ float peso, alt;
+ 
+ for (i=1; i <= 11; i= i+1)
+  
+  printf("\nInforme a sua idade: ");
+  scanf("%i", &idade);
+  
+  printf("\nInforme o seu peso: ");
+  scanf("%f", &peso);
+  
+  printf("\nInforme sua altura: ");
+  scanf("")
+}
